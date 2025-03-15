dee479092f938504dc1d41998f90e34485e58819
Eric <eric.so.dev@gmail.com>
2025-03-15 18:18:22 -0400

    chore(nvim): nvim initial setup

---
+++ nvim/lua/vim/keybinds.lua
@@ -0,0 +1,41 @@
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Move codeblocks vertically
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Maintain cursor position on line append
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Keep visual mode after shifts
+vim.keymap.set('v', '>', '>gv', { desc = 'Keep visual mode after shifts', noremap = true })
+vim.keymap.set('v', '<', '<gv', { desc = 'Keep visual mode after shifts', noremap = true })
+
+-- Yank to void register
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Yank to void register' })
+
+vim.keymap.set('n', '<leader>pc', ':Telescope colorscheme<Enter>', { desc = 'Search colorschemes' })
+
+-- Center after jump
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
