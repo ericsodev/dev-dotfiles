044113fd54001f7d5fa6369a9f03b819c1a6f2de
Eric <eric.so.dev@gmail.com>
2025-03-15 20:47:04 -0400

    feat(nvim): auto pairs and tags

--- nvim/.config/nvim/lua/plugins/core/snacks.lua
+++ nvim/.config/nvim/lua/plugins/core/snacks.lua
@@ -27,14 +27,14 @@ return {
     { '<leader>sg', function() Snacks.picker.grep() end },
     { '<leader>sj', function() Snacks.picker.jumps() end, desc = 'Jumps' },
     { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
+    { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
+    { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
     -- LSP
     { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
     { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
     { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
     { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
     { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
-    { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
-    { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
     -- Other
     { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
     { '<leader>un', function() Snacks.notifier.hide() end, desc = 'Dismiss All Notifications' },
