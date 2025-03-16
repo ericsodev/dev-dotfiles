c2622f906e896741c59dcacd2d3dd4a501e8a980
Eric <eric.so.dev@gmail.com>
2025-03-15 20:38:26 -0400

    feat(nvim): snacks keybinds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,7 +10,7 @@ require 'vim.filetypes'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
@@ -22,6 +22,7 @@ require('lazy').setup {
   spec = {
     { import = 'plugins' },
     { import = 'plugins.themes' },
+    { import = 'plugins.qol' },
     { import = 'plugins.core' },
     { import = 'plugins.lsp' },
   },
--- nvim/.config/nvim/lua/plugins/core/snacks.lua
+++ nvim/.config/nvim/lua/plugins/core/snacks.lua
@@ -1,5 +1,6 @@
+---@module "snacks"
 return {
-  "folke/snacks.nvim",
+  'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
@@ -16,8 +17,26 @@ return {
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = false},
+    scroll = { enabled = false },
     statuscolumn = { enabled = true },
     words = { enabled = true },
+  },
+  keys = {
+    -- Search
+    { '<leader>sf', function() Snacks.picker.files() end },
+    { '<leader>sg', function() Snacks.picker.grep() end },
+    { '<leader>sj', function() Snacks.picker.jumps() end, desc = 'Jumps' },
+    { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
+    -- LSP
+    { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+    { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
+    { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
+    { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
+    { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
+    { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
+    { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
+    -- Other
+    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
+    { '<leader>un', function() Snacks.notifier.hide() end, desc = 'Dismiss All Notifications' },
   },
 }
