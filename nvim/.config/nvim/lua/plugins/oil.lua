b474ea48b7a805208371cf91e993ad2264e030d0
Eric <eric.so.dev@gmail.com>
2025-05-12 00:16:30 -0400

    feat(nvim): only show hidden files if they are tracked for oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,41 @@
+-- helper function to parse output
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, '\n', { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub('/$', '')
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system({ 'git', 'ls-files', '--ignored', '--exclude-standard', '--others', '--directory' }, {
+        cwd = key,
+        text = true,
+      })
+      local tracked_proc = vim.system({ 'git', 'ls-tree', 'HEAD', '--name-only' }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+
+local git_status = new_git_status()
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
@@ -21,8 +59,42 @@ return {
     end
 
     require('oil').setup {
+      default_file_explorer = false,
       view_options = {
-        show_hidden = true,
+        show_hidden = false,
+        is_hidden_file = function(name, bufnr)
+          local dir = require('oil').get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, '.') and name ~= '..'
+
+          local function has_value(tab, val)
+            for _index, value in ipairs(tab) do
+              if value == val then
+                return true
+              end
+            end
+
+            return false
+          end
+
+          local specific_hidden_files = { 'tsconfig.tsbuildinfo', '.eslintcache' }
+
+          if has_value(specific_hidden_files, name) then
+            print(name)
+            return true
+          end
+
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then
+            return is_dotfile
+          end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
       },
       skip_confirm_for_simple_edits = true,
       keymaps = {
