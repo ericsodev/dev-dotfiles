f7928bb6db0e085f0c3a19c0a28c4aba16192801
Eric <eric.so.dev@gmail.com>
2025-05-11 22:08:37 -0400

    feat: add conform css formatting

--- nvim/.config/nvim/lua/plugins/core/conform.lua
+++ nvim/.config/nvim/lua/plugins/core/conform.lua
@@ -5,9 +5,7 @@ return { -- Autoformat
   keys = {
     {
       '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
-      end,
+      function() require('conform').format { async = true, lsp_format = 'fallback' } end,
       mode = '',
       desc = '[F]ormat buffer',
     },
@@ -37,6 +35,7 @@ return { -- Autoformat
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
       typescript = { 'prettierd', 'prettier', stop_after_first = true },
       svelte = { 'prettierd', 'prettier', stop_after_first = true },
       javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
